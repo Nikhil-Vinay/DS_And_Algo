0823ff85a96ed05e645917d5ef105622aa08b040
Nikhil-Vinay <44620175+Nikhil-Vinay@users.noreply.github.com>
2020-08-31 16:52:30 +0530

    committing fenwick_tree related files

---
+++ Fenwick_Tree/xor_fenwick_tree.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void updateTree(int* arr, int* BIT, int n, int index, int val) 
+{
+   arr[index] = val;
+   for(; index <= n; index += (index & (-index))) {
+      BIT[index] ^= val;
+   }
+}
+
+int query (int* BIT, int index)
+{
+   int xor_sum = 0;
+   for(; index > 0; index -= (index & (-index))) {
+       xor_sum ^= BIT[index];
+   }
+   return xor_sum;
+}
+
+int main()
+{
+   int n;
+   cout<<"Enter the number of elements of array: "<<endl;
+   cin>>n;
+   
+   // default construtor will initialize all elements by zero
+   int* arr = new int[n+1]();
+   int* BIT = new int[n+1]();
+   for(int i = 1; i <= n; i++) {
+     int num;
+     cin>>num;
+     updateTree(arr, BIT, n, i, num);
+   }
+
+   cout<<"xor sum from index 0 to index 6 : "<<query(BIT, 6)<<endl;
+   cout<<"xor sum from index 2 to index 5 : "<<(query(BIT, 5)^query(BIT, 1))<<endl;
+
+   // Note: current 1 is at index 1 in array. If we want to update with 5 at index 1
+   // Here it is different from segment tree, previous value(1) is distributed between nodes
+   // so, if we will directly update as below then it will additionaly distribute with new val (5)
+   // to respective nodes so, effectively total value at index(1) will become (1^5 = 6)
+   // updateTree(arr, BIT, n , 1, 5)  // wont work
+   //
+   // So, we can update in different way as: first remove previous value then add new value as below:
+   // updateTree(arr, BIT, n, 1, arr[1]);  // it nullify at index 1 as a^a = 0;
+   // updateTree(arr, BIT, n, 1, 5);
+   //
+   // Or we can updat with relative value of new value from previus value as below:
+   // updateTree(arr, BIT, n, 1, 5^arr[1])  // a^5^a = 5
+
+   updateTree(arr, BIT, n , 1, 5^arr[1]);  // update index 1 with 5
+   cout<<"XOR Sum of range from index 0 to index 5 after update : "<< query(BIT, 5)<<endl;
+   
+   return 0;
+}
